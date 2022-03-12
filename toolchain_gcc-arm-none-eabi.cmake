84dca94e0972b9351f0cacff4618e8a2b2a0250c
Nathan Larsen <n8tlarsen@gmail.com>
2022-03-12 15:05:24 -0600

    Add CMake project and vscode settings

---
+++ toolchain_gcc-arm-none-eabi.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(GCC_PATH "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10/bin")
+set(CMAKE_C_COMPILER   ${GCC_PATH}/arm-none-eabi-gcc.exe CACHE INTERNAL "")
+set(CMAKE_CXX_COMPILER ${GCC_PATH}/arm-none-eabi-g++.exe CACHE INTERNAL "")
+set(CMAKE_ASM_COMPILER ${GCC_PATH}/arm-none-eabi-g++.exe CACHE INTERNAL "")
+set(CMAKE_ASM_FLAGS_INIT "-x assembler-with-cpp")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "--specs=nosys.specs")
+set(CMAKE_OBJCOPY ${GCC_PATH}/arm-none-eabi-objcopy CACHE INTERNAL "")
+set(CMAKE_SIZE    ${GCC_PATH}/arm-none-eabi-size    CACHE INTERNAL "")
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
